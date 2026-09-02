91045ecf58e3f8c03afb43a2500435547786c1ff
Hadrian <hadrian.lazic@gmail.com>
2026-09-02 14:50:24 -0700

    upd

--- main.c
+++ main.c
@@ -1,14 +1,18 @@
 
 enum Opcode {
-  AND,
-  NOT,
-  OR,
-  XOR,
-  ADD,
-  SUB,
-  IMM,
-  SHR,
-  SHL,
+  AND = 0,
+  NOT = 1,
+  OR = 2,
+  XOR = 3,
+  ADD = 4,
+  SUB = 5,
+  MUL = 6,
+  DIV = 7,
+  MOD = 8,
+  IMM = 9,
+  SHR = 10,
+  SHL = 12,
+  POPCNT = 13,
 };
 
 int main() {
